9446220788b2ce5c510f89e1f6c157a4c5040be1
Marcos Sobral <marcossobral94@gmail.com>
2016-09-11 23:54:34 -0300

    Update questao_10_converter_decimal_binario.cpp

--- IFBA/EXERCICOS/LISTA_DE_100_QUESTOES/questao_10_converter_decimal_binario.cpp
+++ IFBA/EXERCICOS/LISTA_DE_100_QUESTOES/questao_10_converter_decimal_binario.cpp
@@ -2,18 +2,18 @@
 
 int main(){
 	
-	int n, x[5], y;
+	int n, x[4], y;
 	
 	printf("Informe um numero menor que 32:");
 	scanf("%d", &n);
 	
-	for(y = 0; y < 5; y++){
+	for(y = 0; y < 4; y++){
 		
 		x[y] = n%2;
 		n = n / 2;
 		
 	}
 	
-	printf("Numero em binario: 000%d%d%d%d%d",x[4],x[3],x[2],x[1],x[0]);
+	printf("Numero em binario: 0000%d%d%d%d",x[3],x[2],x[1],x[0]);
 	
 }
