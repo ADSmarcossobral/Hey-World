7cf2a633a88d3a6689fb04bda63946a6bba668a8
Marcos Sobral <marcossobral94@gmail.com>
2016-09-12 00:01:05 -0300

    Update questao_17_caixa_eletronico.cpp

--- IFBA/EXERCICOS/LISTA_DE_100_QUESTOES/questao_17_caixa_eletronico.cpp
+++ IFBA/EXERCICOS/LISTA_DE_100_QUESTOES/questao_17_caixa_eletronico.cpp
@@ -2,7 +2,7 @@
 
 main(){
 	
-	int valor, nDisp[7]{100, 50, 20, 10, 5, 2, 1}, y[7]{0, 0, 0, 0, 0, 0, 0};
+	int valor, nDisp[7] = {100, 50, 20, 10, 5, 2, 1}, y[7] = {0, 0, 0, 0, 0, 0, 0};
 	
 	scanf("%d", &valor);
 	
@@ -15,17 +15,11 @@ main(){
 			
 		}
 		
-	}
-	
-	for(int w = 0; w < 7; w++){
-		
-		if(y[w] > 0){
+                if(y[x] > 0){
 			
-			printf("\n%d de %d", y[w], nDisp[w]);
+			printf("\n%d de %d", y[x], nDisp[x]);
 			
 		}
-		
 	}
-	
 	
 }
