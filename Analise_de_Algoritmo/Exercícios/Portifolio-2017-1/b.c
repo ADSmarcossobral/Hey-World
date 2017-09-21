3d03a31a93adff8696076172f7d366f63596c994
ADSmarcossobral <marcossobral94@gmail.com>
2017-09-21 11:29:19 -0300

    fibo interativo

--- Analise_de_Algoritmo/Exercícios/Portifolio-2017-1/b.c
+++ Analise_de_Algoritmo/Exercícios/Portifolio-2017-1/b.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 clock_t t0, t1;
 
@@ -14,8 +15,15 @@ int fiboRec(int termo){
         return fiboRec(termo-1) + fiboRec(termo-2);
 }
 
-int fiboInter(int n){
-
+void fiboInter(int n){
+    int *fibo = (int *) malloc(n * sizeof(int));
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for(int x = 0; x < n; x++){
+        printf("%d ", fibo[x]);
+        if(x > 0 && x != n - 1)
+            fibo[x+1] = fibo[x] + fibo[x-1];
+    }
 }
 
 int main(){
@@ -38,5 +46,19 @@ int main(){
         t1 = clock();
     }
     printf("\nTempo de execucao %lf\n", (double) (t1 - t0) / (CLOCKS_PER_SEC));
+    printf("================ FIBONACCI INTERATIVO =================\n");
+    printf("\nOs 5 primeiros termos: ");
+    t0 = clock();
+    fiboInter(cont);
+    t1 = clock();
+    tempo = (double) (t1 - t0) / (CLOCKS_PER_SEC);
+    printf("\nTempo de execucao %lf\n", tempo);
+    /*printf("\nOs termos dos primeiros 20 segundos: ");
+    t1 = 0;
+    t0 = clock();
+    while((double) (t1 - t0) / (CLOCKS_PER_SEC) < 20){
+        
+    }
+    printf("\nTempo de execucao %lf\n", (double) (t1 - t0) / (CLOCKS_PER_SEC));*/
     return 0;
 }
