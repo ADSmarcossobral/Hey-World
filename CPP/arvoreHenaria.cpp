78cec79f00260c7feb287d5a4d0ef5938e0c9f8c
Alex <alexstlopes@gmail.com>
2017-09-22 18:41:41 -0300

    up

--- CPP/arvoreHenaria.cpp
+++ CPP/arvoreHenaria.cpp
@@ -4,14 +4,37 @@ using namespace std;
 
 
 // Representação de uma árvore henária
-struct no{
-    int val;
-    vector<struct no*> adj;
+struct No{
+    int peso;
+    vector<No*> adj;
+};
+
+int maiorSoma(No *r){
+    int maior = -1;
+    queue<No*> fila;
+    fila.push(r);
+    No* atual;
+    while(!fila.empty()){
+        atual = fila.front();
+        fila.pop();
+        for(No* filho : atual->adj){
+            fila.push(filho);
+            int soma = filho->peso + atual->peso;
+            if(maior == -1 || soma > maior)
+                maior = soma;
+        }
+    }
+    return maior;
 }
 
-void percorrer(No *node){
-
+No *add(No* raiz, No* filho){
+    if(raiz == NULL)
+        return filho;
+    else if(){
+        r->adj.push(filho);
+    }
 }
+
 
 
 int main(){
