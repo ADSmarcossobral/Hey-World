d6348c30c9de9e5c8b6922c2b874c202432bc84c
ADSmarcossobral <marcossobral94@gmail.com>
2017-10-17 11:20:46 -0300

    concluindo fibo interativo

--- Analise_de_Algoritmo/Exercícios/Portifolio-2017-1/b.c
+++ Analise_de_Algoritmo/Exercícios/Portifolio-2017-1/b.c
@@ -10,20 +10,23 @@ int fiboRec(int termo){
     }
     else if(termo == 2){
         return 1;
-    }
-    else
+    }else
         return fiboRec(termo-1) + fiboRec(termo-2);
 }
 
-void fiboInter(int n){
+int fiboInter(int n){
     int *fibo = (int *) malloc(n * sizeof(int));
+    if(n == 1)
+        return 0;
+    if(n == 2)
+        return 1;
     fibo[0] = 0;
     fibo[1] = 1;
     for(int x = 0; x < n; x++){
-        printf("%d ", fibo[x]);
         if(x > 0 && x != n - 1)
             fibo[x+1] = fibo[x] + fibo[x-1];
     }
+    return fibo[n-1];
 }
 
 int main(){
@@ -38,27 +41,31 @@ int main(){
     tempo = (double) (t1 - t0) / (CLOCKS_PER_SEC);
     printf("\nTempo de execucao %lf\n", tempo);
     cont = 1;
-    printf("\nOs termos dos primeiros 20 segundos: ");
+    printf("\nOs termos dos primeiros 20 tempos: ");
     t1 = 0;
     t0 = clock();
-    while((double) (t1 - t0) / (CLOCKS_PER_SEC) < 20){
+    while((double) (t1 - t0) / (CLOCKS_PER_SEC) < 0.000020){
         printf("%d ", fiboRec(cont++));
         t1 = clock();
     }
     printf("\nTempo de execucao %lf\n", (double) (t1 - t0) / (CLOCKS_PER_SEC));
-    printf("================ FIBONACCI INTERATIVO =================\n");
+    printf("\n================ FIBONACCI INTERATIVO =================\n");
     printf("\nOs 5 primeiros termos: ");
+    cont = 1;
     t0 = clock();
-    fiboInter(cont);
+    while(cont <= 5)
+        printf("%d ", fiboInter(cont++));
     t1 = clock();
     tempo = (double) (t1 - t0) / (CLOCKS_PER_SEC);
     printf("\nTempo de execucao %lf\n", tempo);
-    /*printf("\nOs termos dos primeiros 20 segundos: ");
+    printf("\nOs termos dos primeiros 20 tempos: ");
+    cont = 1;
     t1 = 0;
     t0 = clock();
-    while((double) (t1 - t0) / (CLOCKS_PER_SEC) < 20){
-        
+    while((double) (t1 - t0) / (CLOCKS_PER_SEC) < 0.000020){
+        printf("%d ", fiboRec(cont++));
+        t1 = clock();
     }
-    printf("\nTempo de execucao %lf\n", (double) (t1 - t0) / (CLOCKS_PER_SEC));*/
+    printf("\nTempo de execucao %lf\n", (double) (t1 - t0) / (CLOCKS_PER_SEC));
     return 0;
 }
