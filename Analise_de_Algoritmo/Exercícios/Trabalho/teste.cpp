cc68720a2981c696297585d5c237d720680eaef2
ADSmarcossobral <marcossobral94@gmail.com>
2017-10-19 18:26:05 -0300

    novos testes

--- Analise_de_Algoritmo/Exercícios/Trabalho/teste.cpp
+++ Analise_de_Algoritmo/Exercícios/Trabalho/teste.cpp
@@ -10,10 +10,16 @@ int main(){
     "caio@gmail.com", "(71) 3311-4124", 8.0, 9, 9);
     teste.inserirAluno("2016116006", "Danilo Ferre", "Travessa Ligel",
     "dan@gmail.com", "(71) 3412-4124", 6.0, 9.5, 6.5);
+    teste.inserirAluno("2016116009", "ADS Man", "Travessa Ligel",
+    "marcossobral94@gmail.com", "(71) 3451-4124", 8.0, 9.5, 9.5);
+    teste.inserirAluno("2016116002", "Alan Turing", "Travessa Java",
+    "caio@gmail.com", "(71) 3311-4124", 8.0, 9, 9);
+    teste.inserirAluno("2016116004", "Renato Soz", "Travessa Ligel",
+    "dan@gmail.com", "(71) 3412-4124", 6.0, 9.5, 6.5);
     // Pré-ordem
     teste.pre_ordem();
     // Consulta
-    teste.buscar("2016116007")->toString();
+    teste.buscar("2016116008")->toString();
     // Media Turma
     cout << "Media turma: " << teste.mediaTurma() << endl;
     // Ordena
