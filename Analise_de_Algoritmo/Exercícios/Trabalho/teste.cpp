c6334ec4441a9783aa2b60e14116850644199826
ADSmarcossobral <marcossobral94@gmail.com>
2017-10-25 15:54:23 -0300

    teste

--- Analise_de_Algoritmo/Exercícios/Trabalho/teste.cpp
+++ Analise_de_Algoritmo/Exercícios/Trabalho/teste.cpp
@@ -4,17 +4,17 @@
 int main(){
     arvore::aluno teste;
     // Inserção
-    teste.inserirAluno("2016116007", "Marcos Sobral", "Travessa Ligel",
+    teste.inserirAluno("2016116008", "Marcos Sobral", "Travessa Ligel",
     "marcossobral94@gmail.com", "(71) 3451-4124", 8.0, 9.5, 9.5);
-    teste.inserirAluno("2016116008", "Caio Jordao", "Travessa Java",
+    teste.inserirAluno("2016116004", "Caio Jordao", "Travessa Java",
     "caio@gmail.com", "(71) 3311-4124", 8.0, 9, 9);
-    teste.inserirAluno("2016116006", "Danilo Ferre", "Travessa Ligel",
+    teste.inserirAluno("2016116010", "Danilo Ferre", "Travessa Ligel",
     "dan@gmail.com", "(71) 3412-4124", 6.0, 9.5, 6.5);
     teste.inserirAluno("2016116009", "ADS Man", "Travessa Ligel",
     "marcossobral94@gmail.com", "(71) 3451-4124", 8.0, 9.5, 9.5);
-    teste.inserirAluno("2016116002", "Alan Turing", "Travessa Java",
+    teste.inserirAluno("2016116015", "Alan Turing", "Travessa Java",
     "caio@gmail.com", "(71) 3311-4124", 8.0, 9, 9);
-    teste.inserirAluno("2016116004", "Renato Soz", "Travessa Ligel",
+    teste.inserirAluno("2016116012", "Renato Soz", "Travessa Ligel",
     "dan@gmail.com", "(71) 3412-4124", 6.0, 9.5, 6.5);
     // Pré-ordem
     teste.pre_ordem();
@@ -25,7 +25,11 @@ int main(){
     // Ordena
     teste.ordena();
     // Deleta
-    teste.removerAluno("2016116007");
+    bool rv = teste.removerAluno("2016116012");
+    if(rv)
+        cout << "Removido" << endl;
+    else
+        cout << "Nao removido" << endl;
     // Pré-ordem
     teste.pre_ordem();
     // Limpa árvore
